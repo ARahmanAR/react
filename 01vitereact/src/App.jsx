029bf3161630ff178e85c0c3299b8ad2168f0473
Abdur Rahman <abdurrahman262023@gmail.com>
2025-11-30 20:39:35 +0600

    Refactor App component to use a variable for username and update main rendering logic to use React.createElement for custom rendering.

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,9 @@
 import MyCustom from './MyCustom'
 function App() {
-
+  const username = "Abdur Rahman"
   return (
     <> {/* This is React Fragment, used to group multiple elements */}
-    <h1>Hello, Vite with React! | Abdur Rahman</h1>
+    <h1>Hello, Vite with React! | {username}</h1>
     <MyCustom />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, mollitia.</p>
     </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,40 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App!</h1>
+//     </div>
+//   )
+// }
+
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'http://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to go to Google'
+// }
+
+// const anotherElement = (
+//   <a href="https://google.com" target='_blank'>visit Google</a>
+// )
+
+const anotherUser = "AR Rahman"
+
+const ReactElement = React.createElement(
+  'a',
+  {href: 'http://google.com', target: '_blank'},
+  'Click me to go to Google', anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    ReactElement
+  
 )
